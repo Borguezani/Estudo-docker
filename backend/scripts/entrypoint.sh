b815b100c0b401f41b8d2d2167be4334483101c0
Daniel Borguezani <daniel_borguezani@hotmail.com>
2025-07-13 11:34:55 -0300

    feat: adiciona configuração do Docker para o backend, incluindo .dockerignore, Dockerfile e ajustes no docker-compose

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
-composer install
+#!/bin/bash
 
-cp .env.example .env
+# Executar migrações (se necessário)
+php artisan migrate --force
 
-php artisan key:generate
-
+# Iniciar o servidor Laravel
 php artisan serve --host=0.0.0.0 --port=80
