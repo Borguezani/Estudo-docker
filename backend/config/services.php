a1c431915e455d0862076820f83be513c243a4f9
Daniel Borguezani <daniel_borguezani@hotmail.com>
2025-07-17 20:44:28 -0300

    feat: adiciona configuração para o serviço TMDB no arquivo de serviços

--- backend/config/services.php
+++ backend/config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'tmdb' => [
+        'api_key' => env('TMDB_API_KEY'),
+        'base_url' => env('TMDB_BASE_URL', 'https://api.themoviedb.org/3'),
+        'image_base_url' => env('TMDB_IMAGE_BASE_URL', 'https://image.tmdb.org/t/p'),
+    ],
+
 ];
